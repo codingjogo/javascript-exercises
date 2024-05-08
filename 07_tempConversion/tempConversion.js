3ce4ac2bf22965a1bfc9aae063510bd67ed9ab39
Jogo <138952391+codingjogo@users.noreply.github.com>
2024-05-08 22:41:48 +0800

    done challenger 07_tempConversion
    - not much it's the same modulus usage

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fa) {
+  const result = Number((fa - 32) * 5/9);
+  return result % 1 !== 0 ? Number(result.toFixed(1)) : result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  // (0°C × 9/5) + 32 = 32°F
+  const result = Number((cel * 9/5) + 32)
+  return result % 1 !== 0 ? Number(result.toFixed(1)) : result;
 };
 
 // Do not edit below this line
