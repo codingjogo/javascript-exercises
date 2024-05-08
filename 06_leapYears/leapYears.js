04736943abca7ff55964030fafbb9a74be6ddac3
Jogo <138952391+codingjogo@users.noreply.github.com>
2024-05-08 22:13:37 +0800

    done challenge 06_leapYears
    - learned how to use modulus
    - learned leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // year divisible by 100 isn't leap year returns false
+    // year divisible by 400 or 4 is a leap year
+    // what's the formula?
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)
 };
 
 // Do not edit below this line
